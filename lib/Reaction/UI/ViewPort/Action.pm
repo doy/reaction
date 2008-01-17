4d6b8127ea4576ed5c541ceec61b274b9dfeedfb
wreis <wreis@03d0b0b2-0e1a-0410-a411-fdb2f4bd65d7>
2008-01-17 02:33:59 +0000

    fixed _build_fields_for_type_Row

--- lib/Reaction/UI/ViewPort/Action.pm
+++ lib/Reaction/UI/ViewPort/Action.pm
@@ -156,7 +156,7 @@ class Action is 'Reaction::UI::ViewPort::Object', which {
 
   #this needs to be fixed. somehow. beats the shit our of me. really.
   #implements build_fields_for_type_Reaction_InterfaceModel_Object => as {
-  implements _build_fields_for_type_DBIx_Class_Row => as {
+  implements _build_fields_for_type_Row => as {
     my ($self, $attr, $args) = @_;
     $self->_build_simple_field(attribute => $attr, class => ChooseOne, %$args);
   };
