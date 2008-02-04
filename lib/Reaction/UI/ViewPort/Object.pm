92112f8888ab7b85a6b844ea7544e9737c27a2e3
edenc <edenc@03d0b0b2-0e1a-0410-a411-fdb2f4bd65d7>
2008-02-04 18:06:38 +0000

    making field_args rw for custom field set up

--- lib/Reaction/UI/ViewPort/Object.pm
+++ lib/Reaction/UI/ViewPort/Object.pm
@@ -22,7 +22,7 @@ class Object is 'Reaction::UI::ViewPort', which {
   has model  => (is => 'ro', isa => IM_Object, required => 1);
   has fields => (is => 'ro', isa => 'ArrayRef', lazy_build => 1);
 
-  has field_args    => (is => 'ro');
+  has field_args    => (is => 'rw');
   has field_order   => (is => 'ro', isa => 'ArrayRef');
 
   has builder_cache   => (is => 'ro', isa => 'HashRef',  lazy_build => 1);
