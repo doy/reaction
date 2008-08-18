062c926abc8c74573a1474a0cf1789242d0e959a
rafl <rafl@03d0b0b2-0e1a-0410-a411-fdb2f4bd65d7>
2008-08-18 20:43:33 +0000

    Revert "coerce VP::SiteLayouts static_base_uri from URI."
    This reverts commit ffbdd3b7365b23f9498eb0c773a1c4bb8cd1b6a6.

--- lib/Reaction/UI/ViewPort/SiteLayout.pm
+++ lib/Reaction/UI/ViewPort/SiteLayout.pm
@@ -6,22 +6,11 @@ use aliased 'Reaction::UI::ViewPort';
 use namespace::clean -except => [ qw(meta) ];
 extends ViewPort;
 
-{
-    use Moose::Util::TypeConstraints qw/subtype where coerce from via/;
 
-    my $str_type = subtype 'Str'    => where { 1 };
-    my $uri_type = subtype 'Object' => where { $_->isa('URI') };
-
-    coerce $str_type
-        => from $uri_type
-        => via { "$_[0]" };
-
-    has 'static_base_uri' => (isa => $str_type, coerce => 1, is => 'rw', lazy_fail => 1);
-
-    no Moose::Util::TypeConstraints;
-}
 
 has 'title' => (isa => 'Str', is => 'rw', lazy_fail => 1);
+
+has 'static_base_uri' => (isa => 'Str', is => 'rw', lazy_fail => 1);
 
 has 'meta_info' => (
        is => 'rw', isa => 'HashRef',
