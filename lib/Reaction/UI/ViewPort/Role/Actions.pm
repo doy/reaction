3be50b19f80d78dc4889fa3cc7586febcbcdcb86
groditi <groditi@03d0b0b2-0e1a-0410-a411-fdb2f4bd65d7>
2008-10-11 23:45:47 +0000

    some more doc patches

--- lib/Reaction/UI/ViewPort/Role/Actions.pm
+++ lib/Reaction/UI/ViewPort/Role/Actions.pm
@@ -76,7 +76,40 @@ A role to ease attaching actions to L<Reaction::InterfaceModel::Object>s
 
 =head2 actions
 
+Automatically built ArrayRef of URI objects pointing to actions
+
 =head2 action_prototypes
+
+A HashRef of prototypes for building the Action links. The prototypes should be
+composed like these:
+
+    my %action_prototypes = (
+      example_action => { label => 'Example Action', uri => $uri_obj },
+    );
+
+    #or you can get fancy and do something like what is below:
+    sub make_label{
+      my($im, $ctx) =  @_; #InterfaceModel::Object/Collection, Catalyst Context
+      return 'label_text';
+    }
+    sub make_uri{
+      my($im, $ctx) =  @_; #InterfaceModel::Object/Collection, Catalyst Context
+      return return $ctx->uri_for('some_action');
+    }
+    my %action_prototypes = (
+      example_action => { label => \&make_label, uri => \&make_uri },
+    );
+
+=head2 action_order
+
+User-provided ArrayRef with how the actions should be ordered eg
+
+     action_order => [qw/view edit delete/]
+
+=head2 computed_action_order
+
+The final computed action order. This may differ from the action_order provided
+if you didn't list all of the actions in that.
 
 =head1 AUTHORS
 
