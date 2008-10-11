7ea09fbbbbed128e4f5e9dc0c0dbafe3fb644b80
groditi <groditi@03d0b0b2-0e1a-0410-a411-fdb2f4bd65d7>
2008-10-11 23:35:34 +0000

    some stub POD

--- lib/Reaction/UI/ViewPort/URI.pm
+++ lib/Reaction/UI/ViewPort/URI.pm
@@ -10,3 +10,35 @@ has display => ( is => 'rw' );
 __PACKAGE__->meta->make_immutable;
 
 1;
+
+__END__;
+
+
+=head1 NAME
+
+Reaction::UI::ViewPort::URI
+
+=head1 DESCRIPTION
+
+Viewport for a URI object
+
+=head1 ATTRIBUTES
+
+=head2 uri
+
+Required URI object representing the URI you wish to point to.
+
+=head2 display
+
+Optional. How this item will be displayed. Current implementations support
+a plain string or a ViewPort object for this value
+
+=head1 AUTHORS
+
+See L<Reaction::Class> for authors.
+
+=head1 LICENSE
+
+See L<Reaction::Class> for the license.
+
+=cut
