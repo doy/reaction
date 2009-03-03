7ccc8c7269babcb98aaf696200251959b3022c4d
wreis <wreis@03d0b0b2-0e1a-0410-a411-fdb2f4bd65d7>
2009-03-03 14:34:43 +0000

    fixed bool field

--- lib/Reaction/UI/ViewPort/Field/Mutable/Boolean.pm
+++ lib/Reaction/UI/ViewPort/Field/Mutable/Boolean.pm
@@ -15,6 +15,12 @@ sub BUILD {
   $self->value(0) unless $self->_model_has_value;
 };
 
+# yeah, bool fields have to always be required
+# otherwise we ever cant get false
+# for reference, take a peek in Field::Role::Mutable::Simple's
+# is_modified logic
+sub value_is_required { 1 }
+
 __PACKAGE__->meta->make_immutable;
 
 
