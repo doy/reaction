bcbb11e940adefa53d7bbe1d67f65957822ea56d
groditi <groditi@03d0b0b2-0e1a-0410-a411-fdb2f4bd65d7>
2009-06-16 20:49:10 +0000

    throw more helpful errors

--- lib/Reaction/InterfaceModel/Action/Role/SimpleMethodCall.pm
+++ lib/Reaction/InterfaceModel/Action/Role/SimpleMethodCall.pm
@@ -1,14 +1,19 @@
 package Reaction::InterfaceModel::Action::Role::SimpleMethodCall;
 
 use Reaction::Role;
-
+use Scalar::Util 'blessed';
 requires '_target_model_method';
 
 sub can_apply { 1; }
 
 sub do_apply {
   my ($self) = @_;
-  $self->target_model->${\$self->_target_model_method};
+  my $object = $self->target_model;
+  my $method_name = $self->_target_model_method;
+  if(my $method_ref = $object->can($method_name)){
+    return $object->$method_ref();
+  }
+  confess("Object ".blessed($object)." doesn't support method ${method_name}");
 }
 
 1;
